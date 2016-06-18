74d5054d181fc807be7a42406230d70300103006
CTSchmidlak <ctschmidlak@gmail.com>
2016-06-18 16:39:23 -0700

    more testing
    I brushed up on a lot of JS stuff, and just tooled around to see how
    general concepts work. Still a little more review to go (while loops,
    objects?) and then I'll dive into trying to understand the Phaser
    library and how to use that stuff. I also swagged out Sublime Text on
    both PC and laptop, so I can now build JS directly in the editor with JS
    Node to see console outputs. So lots of good progress today!

--- src/test.js
+++ src/test.js
@@ -2,3 +2,42 @@
 console.log("whats up TS?");
 console.log("hows it going");
 console.log("Cooper".length);
+
+
+//function practice
+var num = 30;
+
+var div3 = function (x)
+{
+	var value = x/3;
+	console.log(value);
+};
+
+var div6 = function(x)
+{
+	var value = x/6;
+	console.log(value);
+};
+
+div3(num);
+div6(num);
+
+//for loop practice
+console.log("now a for loop...");
+for (x = 1; x < 6; x+=3)
+{
+	console.log(x);
+}
+
+//array practice
+var array = ["fee","fi","fo","fum"]
+var arr2 = "Heteroskedasticity";
+
+console.log(array);
+console.log(arr2[6]);
+
+if (num>29)
+{
+	array.push("englishman");
+	console.log(array);
+}
